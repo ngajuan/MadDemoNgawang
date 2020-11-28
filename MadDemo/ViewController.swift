70db30461b1727c54deba6d85cb83cd9d854bfcc
viciousnga <viciousnga@viciousngas-mbp.fios-router.home>
2020-11-27 20:55:33 -0500

    Initial Commit

---
+++ MadDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadDemo
+//
+//  Created by viciousnga on 11/27/20.
+//  Copyright © 2020 viciousnga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
